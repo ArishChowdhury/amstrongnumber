df7fdbf9b5e7dd216990393e1303e4d7a00762c1
Arish <arishchowdhury.2021@gmail.com>
2024-10-03 23:25:11 +0600

    i did this

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+public class Main {
+    public static void isAmStrong(int number){
+        // step 1: Find the number of digits
+        int digits = (String.valueOf(number)).length();
+        int answer = 0;
+        for (int i = 0; i < ; i++) {
+            
+        }
+
+
+    }
+    public static void main(String[] args) {
+        isAmStrong(232);
+
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
